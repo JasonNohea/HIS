65656671c8e53d637026f60eb91159feb065f48c
Jason Nohea <jason.nohea2002@gmail.com>
2023-12-31 21:29:09 +0700

    adding services page

--- om_hospital/models/__init__.py
+++ om_hospital/models/__init__.py
@@ -1,3 +1,4 @@
 from . import patient
 from . import medicalcheck
 from . import docexamine
+from . import clinicservices
---
+++ om_hospital/models/clinicservices.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+# creating databased
+class ClinicServices(models.Model):
